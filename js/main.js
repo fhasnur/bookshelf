b0c0aba9f7235dcfda3635742e5bc4dabc12a700
fhasnur <fandi.hasnur10@gmail.com>
2023-11-19 05:17:15 +0800

    feat: render book, created elements and handler

--- js/main.js
+++ js/main.js
@@ -15,6 +15,23 @@ document.addEventListener(SAVED_EVENT, function () {
   console.log(localStorage.getItem(STORAGE_KEY));
 });
 
+document.addEventListener(RENDER_EVENT, function () {
+  const uncompletedBookshelfList = document.getElementById('uncompleteBookshelfList');
+  uncompletedBookshelfList.innerHTML = '';
+
+  const completedBookshelfList = document.getElementById('completeBookshelfList');
+  completedBookshelfList.innerHTML = '';
+
+  for (const bookItem of books) {
+    const bookElement = makeBook(bookItem);
+    if (!bookItem.isCompleted) {
+      uncompletedBookshelfList.append(bookElement);
+    } else {
+      completedBookshelfList.append(bookElement);
+    }
+  }
+});
+
 function addBook() {
   const title = document.getElementById('inputBookTitle').value;
   const author = document.getElementById('inputBookAuthor').value;
@@ -57,4 +74,110 @@ function isStorageExist() {
     return false;
   }
   return true;
+}
+
+function makeBook(bookObject) {
+  const textTitle = document.createElement('h3');
+  textTitle.innerText = bookObject.title;
+
+  const textAuthor = document.createElement('h4');
+  textAuthor.innerText = bookObject.author;
+
+  const textYear = document.createElement('p');
+  textYear.innerText = bookObject.year;
+
+  const textWrapper = document.createElement('div');
+  textWrapper.classList.add('book_info');
+  textWrapper.append(textTitle, textAuthor, textYear);
+
+  wrapper = document.createElement('article');
+  wrapper.classList.add('book_item');
+  wrapper.append(textWrapper);
+  wrapper.setAttribute('id', `book_${bookObject.id}`);
+
+  if (bookObject.isCompleted) {
+    const undoButton = document.createElement('button');
+    const wrapperUndoButton = document.createElement('div');
+    wrapperUndoButton.classList.add('undo_button');
+    wrapperUndoButton.append(undoButton);
+
+    undoButton.addEventListener('click', function () {
+      undoBookFromCompleted(bookObject.id);
+    });
+
+    const trashButton = document.createElement('button');
+    const wrapperTrashButton = document.createElement('div');
+    wrapperTrashButton.classList.add('trash_button');
+    wrapperTrashButton.append(trashButton);
+
+    trashButton.addEventListener('click', function () {
+      removeBookFromCompleted(bookObject.id);
+    });
+
+    wrapper.append(wrapperUndoButton, wrapperTrashButton);
+  } else {
+    const checkButton = document.createElement('button');
+    const wrapperCheckButton = document.createElement('div');
+    wrapperCheckButton.classList.add('check_button');
+    wrapperCheckButton.append(checkButton);
+
+    checkButton.addEventListener('click', function () {
+      addBookToCompleted(bookObject.id);
+    });
+
+    wrapper.append(wrapperCheckButton)
+  }
+
+  return wrapper;
+}
+
+function addBookToCompleted(bookId) {
+  const bookTarget = findBook(bookId);
+
+  if (bookTarget == null) return;
+
+  bookTarget.isCompleted = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+
+function removeBookFromCompleted(bookId) {
+  const bookTarget = findBookIndex(bookId);
+
+  if (bookTarget === -1) return;
+
+  books.splice(bookTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+function undoBookFromCompleted(bookId) {
+  const bookTarget = findBook(bookId);
+
+  if (bookTarget == null) return;
+
+  bookTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+function findBook(bookId) {
+  for (const bookItem of books) {
+    if (bookItem.id === bookId) {
+      return bookItem;
+    }
+  }
+
+  return null;
+}
+
+function findBookIndex(bookId) {
+  for (const index in books) {
+    if (books[index].id === bookId) {
+      return index;
+    }
+  }
+
+  return -1;
 }
